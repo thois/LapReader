c34bb29ed41a106dd849d952b09260a137f7bd6c
thois <niko.lindvall@helsinki.fi>
2014-05-23 01:46:29 +0300

    more models

---
+++ LapReader/src/main/java/fi/helsinki/cs/thois/lapreader/model/Heat.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.helsinki.cs.thois.lapreader.model;
+
+/**
+ *
+ * @author niko
+ */
+public class Heat {
+    
+}
